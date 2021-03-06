d2ee0f498c35f2ff0bec95169013cf2ce18af1e1
Bazliel <78105957+Bazliel@users.noreply.github.com>
2021-03-06 20:07:52 +0300

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from kivy.app import App
-from kivy.uix.label import Label
-
-class Demo(App):
-    def build(self):
-        return Label(text="Hello World", font_size=72)
-
-if __name__ == "__main__":
-    Demo().run()
